5ea7204fa835b8ee803d25e9d10b3ad53e8bc701
Fossa <marecocico4@gmail.com>
2021-06-15 20:23:36 +0200

    hope

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var http = require("http");
 })();
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
+var server_host = process.env.YOUR_HOST || 'http://fonok-tools.herokuapp.com';
 	
 	app.listen(server_port, server_host, function() {
 		console.log('Listening on port %d', server_port);
