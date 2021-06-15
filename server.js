d4a78bb256a71ca49a5dd496a376903e4a007c54
Fossa <61390428+Fossaa@users.noreply.github.com>
2021-06-15 15:05:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = process.env.PORT || 8080;
-
-
-
-
-app.send("EZ")
-
-
-app.listen(port, () => {
-    console.log(`Ready!`)
-})
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
