c2aef7f6f0b1fd2ae440482659942cf9dc6b6dba
Fossa <marecocico4@gmail.com>
2021-06-15 20:05:25 +0200

    push

--- index.js
+++ index.js
@@ -59,6 +59,19 @@ function startKeepAlive() {
 }
 
 startKeepAlive();
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8080;
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Ready!`)
+})
 	
 	
 
