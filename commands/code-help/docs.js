fa75de05b38444163b34ca5bb3f44f3701a39f2f
フォサー <panko.marek@yahoo.com>
2021-10-31 23:17:42 +0100

    i kinda tried to add docs command but i was coding on my phone lul
    Yea

---
+++ commands/code-help/docs.js
@@ -0,0 +1,22 @@
+const {SlashCommandBuilder} = require("discord.js")
+const { Discord } = require("discord.js")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+                .setName("docs")
+                .setDescription("Sends discord.js documentation based on your query")
+                        .addStringOption(option => option.setName("query")
+                       .setDescription("What do you want to search in the docs?")
+                      .setRequired(true),
+                            async execute(interaction) {
+                
+                const query = interaction.options.getString("query")
+                const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q${encodeURIComponent(query)}`
+                
+                const docfetch = await fetch(url)
+                const embed = await docfetch.json()
+                
+                interaction.reply({
+                embeds: [embed]
+                })
+                }
