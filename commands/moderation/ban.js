4b9afb5de98de66688a571ead624ced71b00f321
フォサー <61390428+JustFossa@users.noreply.github.com>
2021-11-07 22:32:55 +0000

    Added kick and ban command (working)

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -2,17 +2,20 @@ const {MessageEmbed, Permissions} = require("discord.js")
 const {SlashCommandBuilder} = require("@discordjs/builders")
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("kick")
-        .setDescription("Kicks specified member")
+        .setName("ban")
+        .setDescription("Bans specified member")
         .addUserOption(option => option.setName("target")
-            .setDescription("The user you want to kick")
+            .setDescription("The user you want to ban")
             .setRequired(true))
-        .addStringOption(option => option.setName("reason")
-            .setDescription("Reason you want to kick user for (optional)")
+			.addNumberOption(option => option.setName("length")
+			.setDescription("Amount of days you want the user to ban for ")
+			.setRequired(true))
+.addStringOption(option => option.setName("reason")
+            .setDescription("Reason you want to ban user for (optional)")
             .setRequired(false)),
 
         async execute(interaction, client) {
-					if(!interaction.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) {
+					if(!interaction.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
 						return interaction.reply({
 							content: "You cant use that"
 						})
@@ -20,24 +23,25 @@ module.exports = {
 
 
             const member = interaction.options.getMember("target")
-					
+
+				const length = interaction.options.getNumber("length")
             const why = interaction.options.getString("reason") || "No reason provided"
 
-					if(!member.kickable) {
+					if(!member.bannable) {
 	return interaction.reply({
-		content: "I cannot kick this user"
+		content: "I cannot ban this user"
 	})
 						
 }	
 					const kickEmbed = new MessageEmbed()
-                .setTitle("Member Kicked")
-                .setDescription(`**Member:** ${member} \n **Kicked by:** ${interaction.user} \n **Reason:** \`${why}\``)
+                .setTitle("Member Banned")
+                .setDescription(`**Member:** ${member} \n **Banned by:** ${interaction.user} \n **Reason:** \`${why}\``)
                 .setColor("RED")
                 .setTimestamp()
 
             const dmEmbed = new MessageEmbed()
-                .setTitle("You've been kicked")
-                .setDescription(`**Kicked by:** ${interaction.user} \n **Reason:** \`${why}\` \n **Guild:** \`${interaction.guild}\``)
+                .setTitle("You've been banned")
+                .setDescription(`**Banned by:** ${interaction.user} \n **Reason:** \`${why}\` \n **Guild:** \`${interaction.guild}\``)
                 .setColor("RED")
                 .setTimestamp()
 
@@ -46,7 +50,9 @@ module.exports = {
             })
 
 			
-         await member.kick(why)
+         await member.ban({
+					 days: length, reason: why
+				 })
             
                 interaction.reply({
                     embeds: [kickEmbed]
