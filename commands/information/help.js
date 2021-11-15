da1725e32c50670a2d5b6afe2d1a5dd2d9be41a8
stuvan <darksidervenonika@gmail.com>
2021-11-15 13:02:33 +0530

    help for each individiual command

---
+++ commands/information/help.js
@@ -0,0 +1,67 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require("discord.js")
+const fs = require('fs')
+const path = require('path')
+
+let categories = [];
+let commandInfo = new Map();
+
+fs.readdirSync("./commands").forEach((dir) => {
+    const commands = fs.readdirSync(`./commands/${dir}`).filter((file) => file.endsWith('js'))
+
+    const cmds = commands.map((command) => {
+        let file = require(`../../commands/${dir}/${command}`);
+
+        if (!file.data?.name) return "No command Name"
+
+        let name = file.data.name
+
+        commandInfo.set(file.data.name, file.data.description)
+        return `\`${name}\``;
+    })
+
+    let data = new Object()
+
+    data = {
+        name: dir.toUpperCase(),
+        value: cmds.length === 0 ? "No command" : cmds.join(' ')
+    }
+
+    categories.push(data)
+})
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Replies with help menu!')
+        .addStringOption(option => option.setName('commandname').setDescription('Enter command name')),
+
+    async execute(interaction, client) {
+  
+
+        // check if there's any command passed
+        let option = interaction.options.getString('commandname');
+        if(!option || !commandInfo.get(option)) {
+            const embed = new MessageEmbed()
+            .setTitle(`Help`)
+            .addFields(categories)
+            .setColor('BLURPLE')
+            .setFooter(`Use /help <command> to get detailed info about the command`);
+
+            await interaction.reply({
+                embeds: [embed]
+            })    
+        } else{
+            const embed = new MessageEmbed()
+                .addField(option, commandInfo.get(option))
+                .setColor("BLURPLE")
+                .setFooter(`command help for ${option}`);
+
+                await interaction.reply({
+                    embeds: [embed]
+                }) 
+        }
+        
+    }
+
+}
