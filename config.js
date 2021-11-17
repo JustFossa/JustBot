02e09ac6f535f003fe46c5cb4fa48c284b44dc36
フォサー <61390428+JustFossa@users.noreply.github.com>
2021-11-17 23:45:11 +0000

    Few bug fixes

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ module.exports = {
 		dashboard: {
 		enabled: true, // whether the dashboard is enabled or not
 		secret: "Io7wMV8Iqkn4ruLfCbVyNwGda-xNn5QB", // Your discord client secret
-		baseURL: "http://auth.localhost:42069", // The base URl of the dashboard
+		baseURL: "https://bot.justfossa.tech", // The base URl of the dashboard
 		logs: "910640266686382200", // The channel ID of logs
 		port: 42069, // Dashboard port
 		expressSessionPassword: "Rusnacok7", // Express session password (it can be what you want)
-		failureURL: "http://localhost:42069"
+		failureURL: "https://bot.justfossa.tech"
 	},
 }
