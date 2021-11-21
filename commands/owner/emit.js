30e80bdf29703bb127318c69fc65376a62fadce8
SmugTheKiler <emirgamaleev2009@gmail.com>
2021-11-21 13:29:33 -0500

    Update emit.js

--- commands/owner/emit.js
+++ commands/owner/emit.js
@@ -6,17 +6,16 @@ module.exports = {
 	.setDescription('Emits and Event!')
 	.addSubcommand(command => command.setName("guildmemberadd")
 	  .setDescription("Emits member join event"))
+	
   .addSubcommand(command => command.setName("guildcreate")
     .setDescription("Emits guildCreate command")),
-
+	
    async execute(interaction, client) {
-   
      if(interaction.user.id !== "729224025401851915") interaction.reply({
      content: "You cant use that"
      }) 
      
-     if(interaction.options.getSubcommand() === "guildmemberadd") {
-			 
+     if(interaction.options.getSubcommand() === "guildmemberadd") {	 
      client.emit("guildMemberAdd", interaction.member)
      interaction.reply({
      content: "Event: `guildMemberAdd` was emitted sucessfully"
@@ -24,15 +23,11 @@ module.exports = {
      }
 
      if(interaction.options.getSubcommand() === "guildcreate") {
-			 
       client.emit("guildCreate", interaction.guild)
       interaction.reply({
       content: "Event: `guildCreate` was emitted sucessfully"
       })
       }
    
-   }
-   
-   
-   
+   } 
 }
