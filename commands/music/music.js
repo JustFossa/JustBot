37f3b1d4c20e729c573b5b8e67d25fb8eef8a614
フォサー <panko.marek@yahoo.com>
2021-12-15 22:06:29 +0100

    added complete music system

---
+++ commands/music/music.js
@@ -0,0 +1,132 @@
+const {SlashCommandBuilder, channelMention} = require('@discordjs/builders')
+const {MessageEmbed} = require("discord.js")
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("music")
+        .setDescription("This includes full music system")
+        .addSubcommand(command => command.setName("play")
+            .setDescription("Play a song")
+            .addStringOption(option => option.setName("query")
+                .setDescription("Provide name or url of the song to play")
+                    .setRequired(true)))
+        .addSubcommand(command => command.setName("volume")
+            .setDescription("Adjust the volume")
+            .addNumberOption(option => option.setName("percent")
+                    .setDescription("10 = 10%")
+                        .setRequired(true)))
+        .addSubcommand(command => command.setName("settings")
+            .setDescription("Select an option to configure")
+                .addStringOption(option => option.setName("option")
+                    .setDescription("Select an option")
+                    .addChoice("queue", "queue")
+                    .addChoice("skip", "skip")
+                    .addChoice("pause", "pause")
+                    .addChoice("resume", "resume")
+                    .addChoice("stop", "stop")
+
+                
+                    .setRequired(true))),
+
+async execute(interaction, client) {
+    const {options, member, guild, channel} = interaction
+    const voiceChannel = member.voice.channel
+
+    if(!voiceChannel) {
+        return interaction.reply({
+            content: "You must be in voice channel to be able to use this command",
+            ephemeral: true
+        })
+    }
+
+    if(guild.me.voice.channelId && voiceChannel.id !== guild.me.voice.channelId) {
+        return interaction.reply({
+            content: "I am already playing in another channel",
+            ephemeral: true
+        })
+    }
+
+    try {
+
+
+        switch(options.getSubcommand()) {
+            case "play" : {
+                client.disTube.playVoiceChannel(voiceChannel, options.getString("query"), {textChannel: channel, member: member})
+                return interaction.reply({
+                    content: "🎵 Request received"
+                })
+            }
+            case "volume" : {
+                const volume = options.getNumber("percent")
+                if(volume > 100 || volume < 1) {
+                    return interaction.reply({
+                        content: "You have to specify a number between 1 and 100"
+                    })
+                }
+
+                client.disTube.setVolume(voiceChannel, volume)
+                return interaction.reply({
+                    content: `🔊 Volume has been set to \`${volume}%\``
+                })
+            }
+
+            case "options" : {
+                const queue = await client.disTube.getQueue(voiceChannel)
+
+                if(!queue) {
+                    return interaction.reply({
+                        content: "⛔ There is no queue"
+                    })
+                }
+
+                switch(options.getString("options")) {
+                    case "skip" : {
+                        await queue.skip(voiceChannel)
+                        return interaction.reply({
+                            content: "⏭ song has been skipped"
+                        })
+                    }
+
+                    case "stop": {
+                        await queue.stop(voiceChannel)
+                        return interaction.reply({
+                            content: "⏹ Music has been stopped"
+                        })
+                    }
+
+                    case "pause" : {
+                        await queue.pause(voiceChannel)
+                        return interaction.reply({
+                            content: "⏸ Song has been paused"
+                        })
+                    }
+
+                    case "resume" : {
+                        await queue.resume(voiceChannel)
+                        return interaction.reply({
+                            content: "⏯ song has been resumed"
+                        })
+                    }
+
+                    case "queue": {
+                        return interaction.reply({embeds:[
+                            new MessageEmbed()
+                            .setColor("PURPLE")
+                            .setDescription(`${queue.songs.map(
+                                (song, id) => `\n**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``)}`)
+                        ]})
+                    }
+                }
+                return
+            }
+        }
+
+
+
+
+
+
+
+    } catch(err) {
+        console.log(err)
+    }
+}}
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.aliasses = new Collection()
 client.legacyCommands = new Collection()
 client.usages = new Collection()
 client.dashboard = require("./dashboard/app")
-client.distube = new DisTube(client, {
+client.disTube = new DisTube(client, {
   emitNewSongOnly: true,
   leaveOnFinish: true,
   emitAddSongWhenCreatingQueue: false,
@@ -122,5 +122,30 @@ client.on("messageCreate",async message => {
 })
 
 client.dashboard.load()
+const status = queue => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.join(", ") || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode === 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``
+client.disTube
+    .on("playSong", (queue, song) => queue.textChannel.send(
+        `▶ | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
+    ))
+    .on("addSong", (queue, song) => queue.textChannel.send(
+        `✔ | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+    ))
+    .on("addList", (queue, playlist) => queue.textChannel.send(
+        `🟢 | Added \`${playlist.name}\` playlist (${playlist.songs.length} songs) to queue\n${status(queue)}`
+    ))
+    // DisTubeOptions.searchSongs = true
+    .on("searchResult", (message, result) => {
+        let i = 0
+        message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`)
+    })
+    // DisTubeOptions.searchSongs = true
+    .on("searchCancel", message => message.channel.send(`❌ | Searching canceled`))
+    .on("error", (channel, e) => {
+        channel.send(`❌ | An error encountered: ${e}`)
+        console.error(e)
+    })
+    .on("empty", channel => channel.send("Voice channel is empty! Leaving the channel..."))
+    .on("searchNoResult", message => message.channel.send(`❌ | No result found!`))
+    .on("finish", queue => queue.textChannel.send("Finished!"))
 
 client.login(process.env.TOKEN);
